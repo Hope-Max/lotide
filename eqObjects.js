2b3b299c1b4f43a89f6394a329761c1c3b09adf2
Max Li <hope_max@outlook.com>
2021-07-29 23:46:11 -0400

    Updated by using recursion

--- eqObjects.js
+++ eqObjects.js
@@ -7,17 +7,24 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    for (let key of Object.keys(object1)) {
-      // The value of both keys are arrays
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
-        // The value of both keys are objects
-      } else if (!(object1[key] instanceof Array) && (object1[key] instanceof Object) && !(object2[key] instanceof Array) && (object2[key] instanceof Object)) {
-        return eqObjects(object1[key], object2[key]);
-      } else if (object1[key] !== object2[key]) {
+  } 
+  for (let key of Object.keys(object1)) {
+    // The type of both targets are arrays
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    // The type of both targets are objects
+    } else if (!(object1[key] instanceof Array) && (object1[key] instanceof Object) && !(object2[key] instanceof Array) && (object2[key] instanceof Object)) {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    // The type of both targets are primitive data
+    } else {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
@@ -31,9 +38,21 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-
-    if (array1[i] !== array2[i]) {
-      return false;
+    // The type of both targets are arrays
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    // The type of both targets are objects
+    } else if (!(array1[i] instanceof Array) && (array1[i] instanceof Object) && !(array2[i] instanceof Array) && (array2[i] instanceof Object)) {
+      if (!eqObjects(array1[i], array2[i])) {
+        return false;
+      }
+    // The type of both targets are primitive data
+    } else {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
   }
   return true;
@@ -49,8 +68,8 @@ assertEqual(eqObjects(ab, abc), false);
 const cd = { c: "1", d: ["2", 3]};
 const dc = { d: ["2", 3], c: "1"};
 const cd2 = { c: ["1"], d: ["2", 3]};
-const ef = {e: ["1", 'max'], f: ["2", 3, 'hannah']};
-const fe = {f: ["2", 3, 'hannah'], e: ["1", 'max']};
+const ef = {e: ["1", 'max'], f: ["2", 3, 'hannah'], g: 1};
+const fe = {f: ["2", 3, 'hannah'], e: ["1", 'max'], g: 1};
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
 assertEqual(eqObjects(ef, fe), true);
@@ -62,3 +81,10 @@ const ji = {j: {2: 'hannah', 3: 'Ray'}, i: ['max']};
 
 assertEqual(eqObjects(gh, hg), true);
 assertEqual(eqObjects(ij, ji), false);
+
+assertEqual(eqObjects({a: { z: 1 }, b: 2}, {a: { z: 1 }, b: 2}), true);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+assertEqual(eqObjects({ a: { x: {y: 0, z: 1 }, w: {v: 3, u: 4}}, b: 2}, { a: { x: {y: 0, z: 1 }, w: {v: 3, u: 4}}, b: 2}), true);
