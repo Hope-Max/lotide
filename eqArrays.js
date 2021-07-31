37d69cc909b7ea2bce9da012268dd4a91967deea
Max Li <hope_max@outlook.com>
2021-07-30 20:27:55 -0400

    Updated

--- eqArrays.js
+++ eqArrays.js
@@ -11,18 +11,21 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
+  if (array1.length === 0) {
+    return true;
+  }
   for (let i = 0; i < array1.length; i++) {
-    // The type of both targets are arrays
+    // when both args are arrays
     if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
       if (!eqArrays(array1[i], array2[i])) {
         return false;
       }
-    // The type of both targets are objects
+    // when both args are objects
     } else if (!(array1[i] instanceof Array) && (array1[i] instanceof Object) && !(array2[i] instanceof Array) && (array2[i] instanceof Object)) {
       if (!eqObjects(array1[i], array2[i])) {
         return false;
       }
-    // The type of both targets are primitive data
+    // when 1 or both args are primitive types e.g. num, string, bool
     } else {
       if (array1[i] !== array2[i]) {
         return false;
@@ -33,22 +36,26 @@ const eqArrays = function(array1, array2) {
 };
 
 const eqObjects = function(object1, object2) {
-  
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } 
+  }
+  if (Object.keys(object1).length === 0) {
+    return true;
+  }
+
   for (let key of Object.keys(object1)) {
-    // The type of both targets are arrays
+    // when both args are arrays
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    // The type of both targets are objects
+    // when both args are objects
     } else if (!(object1[key] instanceof Array) && (object1[key] instanceof Object) && !(object2[key] instanceof Array) && (object2[key] instanceof Object)) {
       if (!eqObjects(object1[key], object2[key])) {
         return false;
       }
-    // The type of both targets are primitive data
+    // when 1 or both args are primitive types e.g. num, string, bool
     } else {
       if (object1[key] !== object2[key]) {
         return false;
