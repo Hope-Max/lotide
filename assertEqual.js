b0057147b32f0d204759e744b5cb71cb9efde87a
Max Li <hope_max@outlook.com>
2021-08-08 13:10:42 -0400

    Refactored

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, 10);
-assertEqual('Slack', 'Slack');
+module.exports = assertEqual;
