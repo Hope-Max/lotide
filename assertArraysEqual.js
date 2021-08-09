9b95fc127d433668df1640a60598f49850e317af
Max Li <hope_max@outlook.com>
2021-08-09 01:05:56 -0400

    Refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,12 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (eqArrays(actual, expected)) {
-    console.log('✅✅✅Assertion Passed: ', actual, ' === ', expected);
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log('❌❌❌Assertion Failed: ', actual, ' !== ', expected);
+    console.log(`❌❌❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(['1', '2', '3'], ['1', '2', '3']);
-assertArraysEqual(['1', '2', '3'], ['1', '2', 3]);
+module.exports = assertArraysEqual;
