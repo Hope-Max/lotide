7c9f21d9d1ef1d1c498a132428c043db5d6ead3c
Max Li <hope_max@outlook.com>
2021-08-09 11:45:14 -0400

    Refactored

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log('✅✅✅Assertion Passed: ', actual, ' === ', expected);
-  } else {
-    console.log('❌❌❌Assertion Failed: ', actual, ' !== ', expected);
-  }
-};
-
 const middle = function(array) {
   let output = [];
   let length = array.length;
@@ -31,11 +9,4 @@ const middle = function(array) {
   return output;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(['Lighthouse', 'Labs']), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle(['Lighthouse', 'Labs', 'Bootcamp']), ['Labs']);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+module.exports = middle;
