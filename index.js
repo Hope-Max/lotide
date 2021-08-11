f4b3a4110d80508d7b71a0c306a51d6f451a938f
Max Li <hope_max@outlook.com>
2021-08-10 23:06:32 -0400

    Updated

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const head = require('./head');
 const tail = require('./tail');
-const middle = require('./middle');
-const without = require('./without');
-const takeUntil = require('./takeUntil');
-const map = require('./map');
-const letterPositions = require('./letterPositions');
-const flatten = require('./flatten');
-const findKeyByValue = require('./findKeyByValue');
-const findKey = require('./findKey');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
+const without = require('./without');
+const flatten = require('./flatten');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
 const eqObjects = require('./eqObjects');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
 
 
 module.exports = {
   head,
   tail,
-  middle,
-  without,
-  takeUntil,
-  map,
-  letterPositions,
-  flatten,
-  findKeyByValue,
-  findKey,
-  countLetters,
-  countOnly,
   eqArrays,
-  eqObjects
+  without,
+  flatten,
+  middle,
+  countOnly,
+  countLetters,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  map,
+  takeUntil,
+  findKey
 };
