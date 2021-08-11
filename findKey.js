0da058aac47c9415ee10f9caf0c05bbf652743b9
Max Li <hope_max@outlook.com>
2021-08-11 17:35:01 -0400

    Refactored

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = function(object, callback) {
   for (let key of Object.keys(object)) {
     if (callback(object[key])) {
@@ -14,12 +6,4 @@ const findKey = function(object, callback) {
   }
 };
 
-const actual = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-assertEqual(actual, "noma");
+module.exports = findKey;
