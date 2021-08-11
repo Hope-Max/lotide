7c50483474c19fae90c0a8e1ac35c70b1b9024e6
Max Li <hope_max@outlook.com>
2021-08-11 18:11:26 -0400

    Refactored

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(sentence) {
   let output = {};
   let sentenceWithoutSpace = sentence.split(' ').join('');
@@ -19,13 +11,4 @@ const countLetters = function(sentence) {
   return output;
 };
 
-assertEqual(countLetters('lighthouse in the house')['l'], 1);
-assertEqual(countLetters('lighthouse in the house')['i'], 2);
-assertEqual(countLetters('lighthouse in the house')['g'], 1);
-assertEqual(countLetters('lighthouse in the house')['h'], 4);
-assertEqual(countLetters('lighthouse in the house')['t'], 2);
-assertEqual(countLetters('lighthouse in the house')['o'], 2);
-assertEqual(countLetters('lighthouse in the house')['u'], 2);
-assertEqual(countLetters('lighthouse in the house')['s'], 2);
-assertEqual(countLetters('lighthouse in the house')['e'], 3);
-assertEqual(countLetters('lighthouse in the house')['n'], 1);
+module.exports = countLetters;
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 /*
 const findKeyByValue = function(target, value) {
   for (let key in target) {
@@ -21,12 +14,5 @@ const findKeyByValue = function(target, value) {
     }
   }
 };
-const bestTVShowsByGenre = {
-  sci_fi: 'The Expanse',
-  comedy: 'Brooklyn Nine-Nine',
-  drama: 'The Wire'
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Expanse'), 'sci_fi');
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'), 'comedy');
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+module.exports = findKeyByValue;
