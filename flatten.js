bef05cc919ba72b1ef9bb099d0ef2287ea70ce4f
Max Li <hope_max@outlook.com>
2021-08-11 16:41:10 -0400

    Add recursion and Refactored

--- flatten.js
+++ flatten.js
@@ -1,38 +1,28 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log('✅✅✅Assertion Passed: ', actual, ' === ', expected);
-  } else {
-    console.log('❌❌❌Assertion Failed: ', actual, ' !== ', expected);
-  }
-};
-
 const flatten = function(array) {
-  let output = [];
+  let results = [];
   for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i]) === true) {
-      for (let item of array[i]) {
-        output.push(item);
-      }
+    if (Array.isArray(array[i])) {
+      results = results.concat(flatten(array[i]));
     } else {
-      output.push(array[i]);
+      results.push(array[i]);
     }
   }
-  return output;
+  return results;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, [3, 4], 5, [6]]);
-assertArraysEqual(flatten(['Lighthouse', 'Labs', ['Max', 'Hannah', 'Tom', 'Ray'], 'Bootcamp', ['Niagara Falls', 'GTA']]), ['Lighthouse', 'Labs', ['Max', 'Hannah', 'Tom', 'Ray'], 'Bootcamp', ['Niagara Falls', 'GTA']]);
+// const flatten = function(array) {
+//   let results = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (Array.isArray(array[i])) {
+//       for (let item of array[i]) {
+//         results.push(item);
+//       }
+//     } else {
+//       results.push(array[i]);
+//     }
+//   }
+//   return results;
+// };
+
+module.exports = flatten;
+
